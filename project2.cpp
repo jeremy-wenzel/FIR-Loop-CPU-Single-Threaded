f18fb480879d4e1b50fea669a9b021e586146a5a
Jeremy Wenzel <wenzeljeremy15@gmail.com>
2014-10-12 17:29:05 -0500

    Only grab values at elevation 0 and moved stuff around

--- project2.cpp
+++ project2.cpp
@@ -16,11 +16,19 @@ struct Data{
 void readData(struct Data *d, int size) {
 	FILE* fp = fopen("sub040.data", "r");
 
-	for(int j = 0; j < size; j++) {
-		fscanf(fp, "%f %f %c", &d[j].angle, &d[j].elevation, &d[j].side);
-
-		for(int i = 0; i < 200; i++)
+	bool valid = false;
+	int j = 0;
+	while(fscanf(fp, "%f %f %c", &d[j].angle, &d[j].elevation, &d[j].side) != EOF) {
+		if(d[j].elevation == 0)
+			valid = true;
+		for(int i = 0; i < 200; i++) {
 			fscanf(fp, "%f", &d[j].h[i]);
+			if(valid == false)
+				d[j].h[i] = 0;
+		}
+		if(valid == true)
+			j++;
+		valid = false;
 	}
 	fclose(fp);
 }
@@ -86,36 +94,36 @@ void FIR(float* x, float* y, float* h, int size) {
 
 			// follow example from above
 			// ld2 would be = x[4] x[5] x[6] x[7]
-           		 __m128 ld2 = _mm_load_ps(&x[i-j+3]);	// 200 - 199 + 3 = 200 -196 = 4
+           	__m128 ld2 = _mm_load_ps(&x[i-j+3]);	// 200 - 199 + 3 = 200 -196 = 4
 
 			// r1 = x[2] x[3] x[4] x[5]
-            		__m128 r1 = _mm_shuffle_ps(ld1,ld2, 0x4e);
+            __m128 r1 = _mm_shuffle_ps(ld1,ld2, 0x4e);
 
 			// r0 = x[1] x[2] x[3] x[4]
-	                __m128 r0 = _mm_shuffle_ps(ld1, r1, 0x99);
+	        __m128 r0 = _mm_shuffle_ps(ld1, r1, 0x99);
 		
 			// r2 = x[3] x[4] x[5] x[6]
-            		__m128 r2 = _mm_shuffle_ps(r1, ld2, 0x99);
+            __m128 r2 = _mm_shuffle_ps(r1, ld2, 0x99);
 
 			// r3 = x[4] x[5] x[6] x[7]
-            		__m128 r3 = ld2;
+            __m128 r3 = ld2;
 
 			// r0 = (x[1] x[2] x[3] x[4]) * h[199]
-            		r0 = _mm_mul_ps(r0, h3);
+            r0 = _mm_mul_ps(r0, h3);
 
 			// r1 = (x[2] x[3] x[4] x[5]) * h[198]
-            		r1 = _mm_mul_ps(r1, h2);
+            r1 = _mm_mul_ps(r1, h2);
 
 			// r2 = (x[3] x[4] x[5] x[6]) * h[197]
-	        	r2 = _mm_mul_ps(r2, h1);
+	        r2 = _mm_mul_ps(r2, h1);
 
 			// r3 = (x[4] x[5] x[6] x[7]) * h[196]
-            		r3 = _mm_mul_ps(r3, h0);
+            r3 = _mm_mul_ps(r3, h0);
 
 			sum = _mm_add_ps(sum, r0);
 			sum = _mm_add_ps(sum, r1);
-	        	sum = _mm_add_ps(sum, r2);
-            		sum = _mm_add_ps(sum, r3);
+	        sum = _mm_add_ps(sum, r2);
+            sum = _mm_add_ps(sum, r3);
 
 		}
 		_mm_store_ps(&y[i], sum);
@@ -140,47 +148,36 @@ void combine(int16_t *left, int16_t *right, int16_t *combined, int fileSize) {
 	}
 }
 
-/*
-	This fucntion reads the data from the input stream and puts it into
-	an array
-*/
-
-void do_HRTF_Demo() {
+void steroToHeadphones(Data *d, int size) {
 
 
 }
 
-int main()
-{
-
-	struct Data d[500];
-	printf("Reading from .data file\n");
-	readData(d, 500);		
-	printf("Finished reading from .data file\n");
-	
+/* The actual Demo Driver */
+void do_HRTF_Demo(Data *d, int size) {
 
 	FILE* outFileLeft = fopen("output-80-left.pcm", "w");
 	if(outFileLeft == 0) {
 		fprintf(stderr, "Could not open left output file\n");
-		return(-1);
+		exit(-1);
 	}
 	
 	FILE* outFileRight = fopen("output-80-right.pcm", "w");
 	if(outFileRight == 0) {
 		fprintf(stderr, "Could not open right output file\n");
-		return(-1);
+		exit(-1);
 	}
 	
 	FILE* outFileCombined = fopen("output-80-combined.pcm", "w");
 	if(outFileCombined == 0) {
 		fprintf(stderr, "Could not open combined output file\n");
-		return(-1);
+		exit(-1);
 	}
 	/* HRTF Demo Begin */
 	FILE *inputFile = fopen("raw.pcm", "r");
 	if(inputFile == NULL) {
 		fprintf(stderr, "Could not open the input file\n");
-		return (-1);
+		exit(-1);
 	}
 
 	fseek(inputFile, 0, SEEK_END);		// Go to the end of file
@@ -197,26 +194,23 @@ int main()
 	// Check that the amount read and the fileSize are the same
 	if(read != fileSize) {	
 		fprintf(stderr, "Read is not the same as fileSize");
-		return (-1);
+		exit(-1);
 	}
 	printf("Finished reading from input file\n");
 	// Put input into float x array
 	float* x_input = intToFloat(input, fileSize);
 
-	// Put data into float h array
-	// Probably doing this already
-
 	// Create float array y, aligned to 16
 	float* yLeft = (float*) memalign(16, sizeof(float)*fileSize);
 	if(yLeft == NULL) {
 		fprintf(stderr, "Y Left input memory failed to allocate\n");
-		return (-1);
+		exit (-1);
 	}
 	
 	float* yRight = (float*) memalign(16, sizeof(float)*fileSize);
 	if(yRight == NULL) {
 		fprintf(stderr, "Y Left input memory failed to allocate\n");
-		return (-1);
+		exit(-1);
 	}
 
 	//TODO: actually run with x, h ,y, and fileSize
@@ -240,11 +234,31 @@ int main()
 	floatToInt(yRight, y_right_output, fileSize);
 	int16_t *y_combined = new int16_t[fileSize*2];
 
+	printf("Total samples = %i\n", fileSize*2);
 
 	combine(y_left_output, y_right_output, y_combined, fileSize);
 	// Put y array into file
 	fwrite(y_left_output, sizeof(int16_t), fileSize, outFileLeft);
 	fwrite(y_right_output, sizeof(int16_t), fileSize, outFileRight);
-	fwrite(y_combined, sizeof(int16_t), fileSize, outFileCombined);
+	fwrite(y_combined, sizeof(int16_t), fileSize*2, outFileCombined);
+
+	// Delete Arrays; May screw things up
+	delete[] y_combined;
+	delete[] y_right_output;
+	delete[] y_left_output;
+}
+
+int main()
+{
+
+	struct Data d[100];
+	printf("Reading from .data file\n");
+	readData(d, 100);		
+	printf("Finished reading from .data file\n");
+	do_HRTF_Demo(d, 1000);
+	for(int i = 0; i < 100; i++){
+		printf("%i: Angle = %f  elevation = %f\n", i, d[i].angle, d[i].elevation);
+	}
+	
     return(0);
 }
